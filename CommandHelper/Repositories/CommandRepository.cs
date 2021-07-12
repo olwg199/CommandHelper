e0f1ea528f8cd0d8f362a25eae90caa4d7dd3154
Oleh Yastrebov <olwg199@gmail.com>
2021-07-12 15:23:07 -0400

    Add CommandRepository

---
+++ CommandHelper/Repositories/CommandRepository.cs
@@ -0,0 +1,25 @@
+﻿using CommandHelper.Models;
+using System.Collections.Generic;
+
+namespace CommandHelper.Repositories
+{
+    public class CommandRepository : ICommandRepository
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, Description = "Boil eggs", CMDSnippet = "Boil water", Platform = "Kettle&Pen" },
+                new Command { Id = 0, Description = "Cut bread", CMDSnippet = "Get a knife", Platform = "Knife&chopping board" },
+                new Command { Id = 0, Description = "Make cup of tea", CMDSnippet = "Place teabag in cup", Platform = "Kettle&cup" }
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, Description = "Boil eggs", CMDSnippet = "Boil water", Platform = "Kettle&Pen" }
+        }
+    }
+}
---
+++ CommandHelper/Repositories/ICommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using CommandHelper.Models;
+using System.Collections.Generic;
+
+namespace CommandHelper.Repositories
+{
+    public interface ICommandRepository
+    {
+        IEnumerable<Command> GetAllCommands();
+
+        Command GetCommandById(int id);
+    }
+}
