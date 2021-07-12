41ef1f1ddd915b36ce917cc704c766ffb21ae472
Oleh Yastrebov <olwg199@gmail.com>
2021-07-12 15:05:36 -0400

    Add basic model

---
+++ CommandHelper/Models/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandHelper.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string CMDSnippet { get; set; }
+        public string Platform { get; set; }
+    }
+}
