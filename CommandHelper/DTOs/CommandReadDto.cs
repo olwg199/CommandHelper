7930acaace859f72c7b4aefee87ad8825d59867e
Oleh Yastrebov <olwg199@gmail.com>
2021-07-13 18:04:26 -0400

    Add automapping for Command model

---
+++ CommandHelper/DTOs/CommandReadDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandHelper.DTOs
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string CMDSnippet { get; set; }
+
+        public string Platform { get; set; }
+    }
+}
---
+++ CommandHelper/Profiles/CommandsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CommandHelper.DTOs;
+using CommandHelper.Models;
+
+namespace CommandHelper.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
